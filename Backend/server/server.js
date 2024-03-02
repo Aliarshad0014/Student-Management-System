6b8485bf098aa8e3d670b1acbb34ad4ce6efdb95
Ali Arshad <130868985+Aliarshad0014@users.noreply.github.com>
2024-03-02 18:52:50 +0500

    setting up server.js

---
+++ Backend/server/server.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const app = express();
+
+app.get("/", (req,res) =>{
+    res.status(200).send("welcome")
+
+});
+
+const PORT = 5000;
+app.listen(PORT,()=>{
+    console.log(`server is running at: ${PORT}`)
+})
