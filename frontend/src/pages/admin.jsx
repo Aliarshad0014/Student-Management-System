fd9985ebc4e71c6050994b347625bcf38a3ea0d6
Ali Arshad <130868985+Aliarshad0014@users.noreply.github.com>
2024-03-21 00:50:41 +0500

    Remove the "Add New +" button  comment from the admin page.

--- frontend/src/pages/admin.jsx
+++ frontend/src/pages/admin.jsx
@@ -80,6 +80,3 @@ const AdminPage = ({ campuses, courses, departments, programs, staff, students }
 };
 
 export default AdminPage;
-
-
-{/* <button className="px-4 py-2 bg-green-500 hover:bg-green-600 text-white rounded ">Add New +</button> */}
